f5ccad0ce218d1cfb639a9b19ea5ccfb5a901b6a
Aitor <127235588+aitorGonzalo@users.noreply.github.com>
2024-10-19 19:41:45 +0200

    Delete entradasplus directory

--- entradasplus/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Empresa, Evento, Entrada, Pedido
-
-admin.site.register(Empresa)
-admin.site.register(Evento)
-admin.site.register(Entrada)
-admin.site.register(Pedido)
